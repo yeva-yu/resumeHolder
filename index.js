35c3253c2b4b2eb65c61fcb67fd29ff59b366354
WyattWismer <crazy123frog@gmail.com>
2017-01-29 10:51:36 -0500

    final commit

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ var config = {
 };
 firebase.initializeApp(config);
 var storage = firebase.storage();
+
+var db = admin.database();
+var ref = db.ref("server/saving-data/fireblog/posts");
+
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
